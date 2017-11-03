9728db61a7073168aa7c3d98ac369a0bf1518d72
faketurn <faketurn@gmail.com>
2017-11-03 17:35:57 +0900

    add move turn enemy

--- src/EnemyBase.java
+++ src/EnemyBase.java
@@ -3,4 +3,9 @@ public class EnemyBase extends Enemy {
 	public EnemyBase(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 	}
+	public void move() {
+		super.move();
+		if (x < 100) { vx = 1; }
+		if (300 < x) { vx = -1; }
+	}
 }
